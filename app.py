3d4fa9aa3a12665c2b64f4fc63f9871af6e88b7d
Zach <zach.avant89@gmail.com>
2020-08-08 14:39:32 -0500

    major changes

--- app.py
+++ app.py
@@ -1,67 +1,75 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = "key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
+
 class Tracker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    expenses = db.Column(db.String(300), nullable=False)
+    expenses = db.Column(db.String(300), nullable=True)
     name = db.Column(db.String(50))
     location = db.Column(db.String(50))
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
+    def __init__(self, expenses, name, location):
+        self.expenses = expenses
+        self.name = name
+        self.location = location
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route('/', methods=['POST', 'GET'])
-def main():
+
+@app.route('/')
+def index():
+    all_data = Tracker.query.all()
+    return render_template('main.html', trackers=all_data)
+
+
+@app.route('/insert', methods=['POST', 'GET'])
+def insert():
     if request.method == 'POST':
-        expense_content = request.form['expenses']
-        new_expense = Tracker(expenses=expense_content)
-        
-        try:
-            db.session.add(new_expense)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
-    else:
-        tasks = Tracker.query.order_by(Tracker.date_created).all()
-        return render_template('main.html', tasks = tasks )
+        expenses = request.form['expenses']
+        name = request.form['name']
+        location = request.form['location']
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Tracker.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
+        my_data = Tracker(expenses, name, location)
+        db.session.add(my_data)
         db.session.commit()
-        return redirect('/')
-    except:
-        return 'Error deleting task'
+
+        flash("Expense Successfully Added")
+        return redirect(url_for('index'))
 
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Tracker.query.get_or_404(id)
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+
     if request.method == 'POST':
-        task.expenses = request.form['expenses']
+        my_data = Tracker.query.get(request.form.get('id'))
 
-        try: 
-            db.session.commit()
-            return redirect('/')
-        except: 
-            return 'Error updating'
-    else:
-        return render_template('update.html', task=task)
+        my_data.expenses = request.form['expenses']
+        my_data.name = request.form['name']
+        my_data.location = request.form['location']
 
+        db.session.commit()
+        flash("Records Sucessfully Updated")
+        return redirect(url_for('index'))
+
+
+@app.route('/delete/<id>/', methods=['GET', 'POST'])
+def delete(id):
+    my_data = Tracker.query.get(id)
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Expense Deleted")
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
--- static/login-page.js
+++ static/login-page.js
@@ -1,16 +1,24 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
-const loginSuccessMsg = document.getElementById("login-success-msg")
+const loginSuccessMsg = document.getElementById("login-success-msg");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
-        loginSuccessMsg.style.opacity =1;
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-})
+  if (username === "user" && password === "web_dev") {
+    loginSuccessMsg.style.opacity = 1;
+  } else {
+    loginErrorMsg.style.opacity = 1;
+  }
+});
+
+function validateCode() {
+  var code = document.getElementById("submit").value;
+
+  if (username === "user" && password === "web_dev") {
+    window.location.href = "./main.html";
+  }
+}
